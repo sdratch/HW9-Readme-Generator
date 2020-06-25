4f21f5939ccf03a213b36157b2212fb72bdaa632
Sam Dratch <dratch.sam1@gmail.com>
2020-06-25 17:27:29 -0400

    added questions and prompts

--- index.js
+++ index.js
@@ -1,15 +1,81 @@
+const inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
-
+  "Enter Project Title: ",
+  "Enter Github Username: ",
+  "Enter Email Address: ",
+  "Enter Project Description: ",
+  "Enter Installation Instructions: ",
+  "Enter Usage Information: ",
+  "Select Licences: ",
+  "Enter Contribution: ",
+  "Enter Test Instructions: ",
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-
+  const inquirer = require("inquirer");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: questions[0],
+        name: "title",
+      },
+      {
+        type: "input",
+        message: questions[1],
+        name: "username",
+      },
+      {
+        type: "input",
+        message: questions[2],
+        name: "email",
+      },
+      {
+        type: "input",
+        message: questions[3],
+        name: "description",
+      },
+      {
+        type: "input",
+        message: questions[4],
+        name: "install",
+      },
+      {
+        type: "input",
+        message: questions[5],
+        name: "usage",
+      },
+      {
+        type: "list",
+        message: questions[6],
+        name: "licences",
+        choices:[
+            "MIT",
+            "GNU GPLv3",
+            "Apache License 2.0",
+            "ISC License"
+        ]
+      },
+      {
+        type: "input",
+        message: questions[7],
+        name: "contribution",
+      },
+      {
+        type: "input",
+        message: questions[8],
+        name: "test",
+      },
+    ])
+    .then(function (response) {
+      console.log(response);
+    });
 }
 
 // function call to initialize program
@@ -19,7 +85,7 @@ init();
 //ask for dependences and have npm i as the option
 //## for sections start
 // * # for table of contents
-//when talking about 
+//when talking about
 //questions at end with name and github link
 //usage:use at your own risk
 //a lot of defaults for this
