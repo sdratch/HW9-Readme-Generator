1ecb88563de22fdd85b88d3d4f9e5d08d1e0c237
Sam Dratch <dratch.sam1@gmail.com>
2020-06-25 16:44:13 -0400

    added base files

---
+++ index.js
@@ -0,0 +1,27 @@
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
+
+//create git hub project
+//ask for dependences and have npm i as the option
+//## for sections start
+// * # for table of contents
+//when talking about 
+//questions at end with name and github link
+//usage:use at your own risk
+//a lot of defaults for this
+
+//hardcode first
